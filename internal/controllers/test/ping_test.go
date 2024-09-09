6adad80131775cdc98f805529efb5d11fb0c13bd
炳源 <bingyuango@gmail.com>
2024-09-10 00:15:02 +0800

    feat: 规范了测试函数

--- internal/controllers/test/ping_test.go
+++ internal/controllers/test/ping_test.go
@@ -12,12 +12,11 @@ import (
 
 // 每个接口的对应单点测试应编写在 /internal/controllers/test/xxxx_test.go 中
 
-func TestPing(t *testing.T) {
-	router := gin.Default()
-	router.GET("/ping", api.Ping)
+func testPing(t *testing.T, r *gin.Engine) {
+	t.Helper()
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/ping", nil)
-	router.ServeHTTP(w, req)
+	r.ServeHTTP(w, req)
 
 	assert.Equal(t, http.StatusOK, w.Code)
 	expectedBody := gin.H{
@@ -26,4 +25,10 @@ func TestPing(t *testing.T) {
 	var actualBody gin.H
 	_ = json.Unmarshal(w.Body.Bytes(), &actualBody)
 	assert.Equal(t, expectedBody, actualBody)
+}
+
+func TestPing(t *testing.T) {
+	router := gin.Default()
+	router.GET("/ping", api.Ping)
+	testPing(t, router)
 }
--- internal/router/router_test.go
+++ internal/router/router_test.go
@@ -5,5 +5,4 @@ import "testing"
 // 路由的整体集成测试在 /internal/router/router_test 进行，测试整体的功能
 
 func TestRouter(t *testing.T) {
-
 }
