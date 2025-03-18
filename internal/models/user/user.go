a788a7595d4e977b623f6cd2076ac7e5cbf26c71
炳源 <bingyuango@gmail.com>
2025-03-18 11:01:04 +0800

    chore

--- internal/models/user/user.go
+++ internal/models/user/user.go
@@ -9,5 +9,5 @@ type User struct {
 	Username string `gorm:"column:username;comment:'用户名'"`
 	Password string `gorm:"column:password;comment:'密码'"`
 	Email    string `gorm:"column:email;comment:'email/唯一标识符'"`
-	Role     string `gorm:"column:role;comment:'角色，现在只有admin与user';default:user"`
+	Role     string `gorm:"column:role;comment:'角色，admin与user';default:user"`
 }
