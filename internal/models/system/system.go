22459edc7cc5b160543c5601e291493d1bbce18f
炳源 <bingyuango@gmail.com>
2025-03-27 17:43:45 +0800

    feat: feedback api

--- internal/models/system/system.go
+++ internal/models/system/system.go
@@ -1,6 +1,9 @@
 package system
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Notification struct {
 	gorm.Model
@@ -11,5 +14,6 @@ type Notification struct {
 
 type Feedback struct {
 	gorm.Model
-	Content string `gorm:"column:content"`
+	Content   string    `gorm:"column:content;comment:'反馈内容'" json:"content"`
+	CreatedAt time.Time `json:"created_at"`
 }
